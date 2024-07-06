27345bd847c8041b38566b1b213e1e27c275e56c
khan1203 <rk.rifat1203@gmail.com>
2024-07-06 09:55:23 +0600

    mid 3 solvedC

--- mid/03.cpp
+++ mid/03.cpp
@@ -1,27 +1,134 @@
-//by Khan Muhammad Rifat (CSE, BUBT)
+// by Khan Muhammad Rifat (CSE, BUBT)
 #include <bits/stdc++.h>
 #define ll long long
 #define nl endl
-#define PI 2*acos(0.0)
+#define PI 2 * acos(0.0)
 using namespace std;
 
-void Fast_io(){
+void Fast_io()
+{
     ios_base::sync_with_stdio(false);
     cout.tie(0);
     cin.tie(0);
 }
 
-void solve(){
-    Fast_io();
-    
-}
+class Node
+{
+public:
+    string data;
+    Node *next;
+    Node *prev;
+    Node(string str) : data(str), next(NULL), prev(NULL) {}
+};
 
-int main(){
+class DoublyLinkedList
+{
+public:
+    Node *head;
+    Node *tail;
+    Node *curr;
 
-    // solve();
+    DoublyLinkedList() : head(nullptr), tail(nullptr), curr(nullptr) {}
 
-    int t;
-    cin>>t;
-    while(t--){ solve(); }
-return 0;
+    void insert(string &str)
+    {
+        Node *newNode = new Node(str);
+        if (head == nullptr)
+        {
+            head = tail = curr = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    void visit(string address)
+    {
+        Node *temp = head;
+        while (temp)
+        {
+            if (temp->data == address)
+            {
+                curr = temp;
+                cout << curr->data << nl;
+                return;
+            }
+            temp = temp->next;
+        }
+        cout << "Not Available" << nl;
+    }
+
+    void next()
+    {
+        if (curr->next == nullptr)
+        {
+            cout << "Not Available" << nl;
+        }
+        else
+        {
+            curr = curr->next;
+            cout << curr->data << nl;
+        }
+    }
+
+    void prev()
+    {
+        if (curr->prev == nullptr)
+        {
+            cout << "Not Available" << nl;
+        }
+        else
+        {
+            curr = curr->prev;
+            cout << curr->data << nl;
+        }
+    }
+};
+
+int main()
+{
+    DoublyLinkedList dll;
+
+    while (1)
+    {
+        string add;
+        cin >> add;
+        if (add == "end")
+        {
+            break;
+        }
+        dll.insert(add);
+    }
+
+    // Node* temp2 = dll.head;
+    // while(temp2 != nullptr){
+    //     cout<<temp2->data<<" ";
+    //     temp2 = temp2->next;
+    // }
+
+    int q;
+    cin >> q;
+
+    while (q--)
+    {
+        string cmd;
+        cin >> cmd;
+        if (cmd == "visit")
+        {
+            string address;
+            cin >> address;
+            dll.visit(address);
+        }
+        else if (cmd == "next")
+        {
+            dll.next();
+        }
+        else if (cmd == "prev")
+        {
+            dll.prev();
+        }
+    }
 }
