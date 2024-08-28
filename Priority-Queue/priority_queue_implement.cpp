4d140cc56f83a708c6850bcbb6d93193427e0ec0
khan1203 <rk.rifat1203@gmail.com>
2024-08-28 17:48:40 +0600

    custom-compare-class addedC

--- Priority-Queue/priority_queue_implement.cpp
+++ Priority-Queue/priority_queue_implement.cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 int main()
 {
-    priority_queue<int> pq;
+    priority_queue<int> pq;                             // ->  this is for max-heap (Descending order)
+    priority_queue<int, vector<int>, greater<int>> pq2; // ->  this is for min-heap (Ascending order)
 
     while (1)
     {
@@ -18,17 +19,18 @@ int main()
         {
             int x;
             cin >> x;
-            pq.push(x);
+            pq.push(x); // O(logN)
         }
         else if (opt == 2)
         {
-            pq.pop();
+            pq.pop(); // O(logN)
         }
         else if (opt == 3)
         {
-            cout<<pq.top()<<endl;
+            cout << pq.top() << endl; // O(1)
         }
-        else if(opt == 4){
+        else if (opt == 4)
+        {
             break;
         }
     }
